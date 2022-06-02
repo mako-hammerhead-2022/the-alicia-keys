2dd285553b575a1b2986e12814f5ce4b5a4b8132
Rupert Denton <rupert@Ruperts-MacBook-Pro.local>
2022-06-02 17:02:54 +1200

    wired up JS/CSS/HTML trying to trigger sound onclick

---
+++ index.js
@@ -0,0 +1,22 @@
+const playNote = () => {
+let audio = new Audio('sounds/c-major.wav')
+audio.play()
+}
+
+
+
+
+// document.getElementById('mole').remove()
+// moleRandomizer()
+// })
+
+{/* <audio id="audio" src="http://www.soundjay.com/button/beep-07.wav" autoplay="false" ></audio>
+<a onclick="playSound();"> Play</a>
+<script>
+  function playSound() {
+  var sound = document.getElementById("audio");
+  sound.play();
+} */}
+
+
+document.getElementById('test-button').addEventListener('click', playNote())
