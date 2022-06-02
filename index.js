95c048daaa9d62eece8e43990955684fecee6d34
rupert <rupertdenton1@gmail.com>
2022-06-03 09:36:01 +1200

    finished notes for piano

--- index.js
+++ index.js
@@ -2,65 +2,41 @@
 function playAudio(note){
   let audio
   console.log(note)
-  if(note === 'c-5'){
-    audio = new Audio(`sounds/$c-5.wav`) //'sounds/ + __ + '-major.wav'
-  }
-  audio = new Audio(`sounds/${note}.wav`) //'sounds/ + __ + '-major.wav'
+  audio = new Audio(`sounds/${note}.wav`) 
   audio.play()
 }
 
-
 function playNote(e) {
-  
   let key = e.keyCode
   console.log(key)
-  if (key =='65') { //works
+  if (key =='65') { // keyboard a / piano C x
     return playAudio('c')
   }
-  if (key == '83') { //works
+  if (key == '83') {  // keyboard s / piano D x
     return playAudio('d')
   }
-  if (key == '68'){
+  if (key == '68'){  // keyboard d / piano E x
     return playAudio('e')
   }
-  if (key == '70'){
-    console.log(key)
+  if (key == '70'){ // keyboard f / piano F x
     return playAudio('f')
   }
-  if (key == '74'){
-    console.log(key)
+  if (key == '74'){ // keboard j / piano G x
     return playAudio('g')
   }
-  if (key == '75'){
+  if (key == '75'){ // keyboard k / piano A x
     return playAudio('a')
   }
-  if (key == '76'){
+  if (key == '76'){ // keboard l / piano B x
     return playAudio('b')
   }
-  if (key == '186'){
+  if (key == '186'){ // keyboard ; / piano C1 x
     return playAudio('c-5')
   }
-  }
+}
   
 document.addEventListener("keydown", playNote)
- 
-  // // if (key == '65') { // keyboard a / piano C x
-  // } else if (key == '83') { // keyboard s / piano D x
-    
-  // } else if (key == '68') { // keyboard d / piano E x
-    
-  // } else if (key == '70') { // keyboard f / piano F x
 
-  // } else if (key == '74') { // keboard j / piano G x
-    
-  // } else if (key == '75') { // keyboard k / piano A x
-    
-  // } else if (key == '76') { // keboard l / piano B x
-    
-  // } else if (key == '186') { // keyboard ; / piano C1 x
-    
-  // }
-  
 
 
 
