72d4dd09d847f3f0a601bc8e9be016143058f3c0
Luis Alfonso A. Leh <luisleh@Luiss-MacBook-Pro.local>
2022-06-02 15:16:33 +0800

    added target keys

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ audio.play()
 }
 
 // CODE FROM JSC - DRESS-THE-CLOWN
-// onkeydown for (a, s, d, e, f, j, k, l, ;)
+// onkeydown for (a = 65, s=83, d=68, f=70, j=74, k=75, l=76, ;=)
 function checkKey(e) {
 
+  console.log(e)
+
+  if (e.keyCode == '65') { // keyboard a / piano C
+  } else if (e.keyCode == '83') { // keyboard s / piano D
+    
+  } else if (e.keyCode == '68') { // keyboard d / piano E
+    
+  } else if (e.keyCode == '70') { // keyboard f / piano F
+
+  } else if (e.keyCode == '74') { // keboard j / piano G
+    
+  } else if (e.keyCode == '75') { // keyboard k / piano A
+    
+  } else if (e.keyCode == '76') { // keboard l / piano B
+    
+  } else if (e.keyCode == '186') { // keyboard ; / piano C1
+    
+  }
+
 }
+
+
 
 
 
